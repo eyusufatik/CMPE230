f5fd98e2bd01af5ceb8d6a1cdc3b2dccee2ae203
eyusufatik <esadyusufatik@gmail.com>
2022-05-25 19:59:41 +0300

    final

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     int rc;
     QApplication app(argc, argv);
 
-    Calculator *calc = new Calculator;
+    Calculator *calc = new Calculator();
 
     calc->show();
 
